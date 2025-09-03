07cd5c1a3aa8aac25755f8446e66aede1a1a71f4
Prashant Pandey <prashantthestar04@gmail.com>
2025-09-03 23:08:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "RG1" {
-  name     = "sho-rg"
+resource "azurerm_resource_group" "RG2" {
+  name     = "sho1-rg"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "STG" {
-  depends_on = [ azurerm_resource_group.RG1 ]
-  name                     = "shostg"
-  resource_group_name      = azurerm_resource_group.RG1.name
-  location                 = azurerm_resource_group.RG1.location
+  depends_on = [ azurerm_resource_group.RG2 ]
+  name                     = "shostg123"
+  resource_group_name      = azurerm_resource_group.RG2.name
+  location                 = azurerm_resource_group.RG2.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
@@ -16,17 +16,17 @@ resource "azurerm_storage_account" "STG" {
   }
 }
 
-resource "azurerm_resource_group" "RG2" {
-  name     = "sho1-rg"
+resource "azurerm_resource_group" "RG3" {
+  name     = "sho12-rg"
   location = "West Europe"
 }
 
 
 resource "azurerm_virtual_network" "vnet" {
-  depends_on = [ azurerm_resource_group.RG1 ]
+  depends_on = [ azurerm_resource_group.RG2 ]
   name                = "vnet1"
-  location            = azurerm_resource_group.RG1.location
-  resource_group_name = azurerm_resource_group.RG1.name
+  location            = azurerm_resource_group.RG2.location
+  resource_group_name = azurerm_resource_group.RG2.name
   address_space       = ["10.0.0.0/16"]
   
 
